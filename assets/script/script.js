3b8d77607927a685fd351b7ac276167c2074155b
Dimitar Cholakov <dcholakov4@gmail.com>
2021-12-06 15:07:55 +0100

    Add logic to Go To Top button.

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,21 @@
 let scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#my-nav'
   })
+
+// Go to top button
+const btnTop = document.getElementById("toTopBtn");
+
+window.onscroll = function() {scrollingDown()};
+function scrollingDown() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+}
+
+function goToTop() {
+  window.scrollTo(0,0);
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+}
