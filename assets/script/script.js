0adcbb654dd2c33037ef06bb75cc23ec8ef9ec5c
Dimitar Cholakov <dcholakov4@gmail.com>
2022-01-17 15:42:31 +0100

    Remove read-more js functionality.

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,24 +14,6 @@ function goToTop() {
   window.scrollTo(0,0);
 }
 
-// Read more button
-function moreInfo() {
-  let ellipsis = document.getElementById("ellipsis");
-  let showMore = document.getElementById("more-text");
-  let readBtn = document.getElementById("show-btn");
-
-  if (ellipsis.style.display === "none") {
-    ellipsis.style.display = "inline";
-    readBtn.innerHTML = "<em>Read more</em> <i class='fas fa-chevron-circle-down'></i>";
-    showMore.style.display = "none";
-  } else {
-    ellipsis.style.display = "none";
-    readBtn.innerHTML = "<em>See less</em> <i class='fas fa-chevron-circle-up'></i>";
-    showMore.style.display = "inline";
-  }
-}
-
-
 // Bootstrap popovers
 const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
